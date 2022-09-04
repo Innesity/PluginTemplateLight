37d5b9eb80ae019872bd63660acea95ffdb3b47c
Stefan Covaci <flintclay6@gmail.com>
2022-09-04 13:18:20 +0300

    Update Start.java

--- src/main/java/org/mineacademy/template/Start.java
+++ src/main/java/org/mineacademy/template/Start.java
@@ -4,14 +4,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.mineacademy.fo.plugin.SimplePlugin;
 
-
-/**
- * PluginTemplate is a simple template you can use every time you make
- * a new plugin. This will save you time because you no longer have to
- * recreate the same skeleton and features each time.
- *
- * It uses Foundation for fast and efficient development process.
- */
 public final class Start extends SimplePlugin {
 
 	@Override
